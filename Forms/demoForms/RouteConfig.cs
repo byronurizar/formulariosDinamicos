d6828bbca3dcf3e7725327d08193f32b277898dd
Byron Lopez Urizar <blopez@maycom-ad.maycom.com.gt>
2020-01-22 17:22:05 -0600

    Cambios s

---
+++ Forms/demoForms/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+namespace demoForms
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+
+        {
+
+            routes.EnableFriendlyUrls();
+            routes.MapPageRoute("", "formulario", "~/formulario.aspx");
+            routes.MapPageRoute("", "Customers", "~/Customers.aspx");
+            routes.MapPageRoute("GetCustomer","GetCustomer/{CustomerId}","~/Customers.aspx");
+            routes.MapPageRoute("", "Products", "~/Products.aspx");
+            routes.MapPageRoute("GetProduct", "GetProduct/{ProductId}","~/Products.aspx");
+
+        }
+    }
+}
